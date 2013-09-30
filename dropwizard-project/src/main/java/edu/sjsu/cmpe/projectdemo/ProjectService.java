48b20f2719b8d9a98cb4b9bdc8a58d78e3b1850f
jeenathomas6 <jeena.at@gmail.com>
2013-09-30 02:56:19 -0700

    Update ProjectService.java

--- dropwizard-project/src/main/java/edu/sjsu/cmpe/projectdemo/ProjectService.java
+++ dropwizard-project/src/main/java/edu/sjsu/cmpe/projectdemo/ProjectService.java
@@ -4,11 +4,9 @@ import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
 
-<<<<<<< HEAD
+
 import edu.sjsu.cmpe.projectdemo.api.resources.*;
-=======
 import edu.sjsu.cmpe.projectdemo.api.resources.PortalResource;
->>>>>>> ba43f4bedbe31adfd55f9efe660d796254bbe790
 import edu.sjsu.cmpe.projectdemo.config.ProjectServiceConfiguration;
 
 public class ProjectService extends Service <ProjectServiceConfiguration>{
@@ -27,11 +25,9 @@ public class ProjectService extends Service <ProjectServiceConfiguration>{
 	public void run(ProjectServiceConfiguration configuration,
 			Environment environment) throws Exception {
 
-<<<<<<< HEAD
 		environment.addResource(LoginResource.class);
-=======
->>>>>>> ba43f4bedbe31adfd55f9efe660d796254bbe790
 		environment.addResource(PortalResource.class);
+		environment.addResource(ClinicResource.class);
 		
 	}
 	
